fdd1aa20f1f6b9f48f9632e5b2fe7d586e90213d
1246030David <115796157+1246030David@users.noreply.github.com>
2023-02-24 16:08:29 +0000

    Initial commit

---
+++ app/components/navbar/navbarlink.tsx
@@ -0,0 +1,19 @@
+import { Link } from "@remix-run/react";
+
+type NavbarLinkProps = {
+  children: React.ReactNode;
+  url: string;
+};
+
+const NavbarLink = ({ children, url }: NavbarLinkProps) => {
+  return (
+    <Link
+      to={url}
+      className="mt-1 block rounded px-2 py-1 font-semibold text-white hover:bg-slate-800 hover:text-gray-500 md:ml-2"
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default NavbarLink;
